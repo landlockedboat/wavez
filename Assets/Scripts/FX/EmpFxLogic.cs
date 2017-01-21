915c0e5e780de68e0d0be8084c60994562ddc23b
wextia <valcazar96@gmail.com>
2017-01-21 22:39:06 +0200

    Added som sounds

--- Assets/Scripts/FX/EmpFxLogic.cs
+++ Assets/Scripts/FX/EmpFxLogic.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EmpFxLogic : MonoBehaviour {
+    AudioSource auS;
+    [SerializeField]
+    float minPitch = 0.5f;
+    [SerializeField]
+    float maxPitch = 1.5f;
+
     ParticleSystem ps;
 	void Awake() {
         ps = GetComponent<ParticleSystem>();
-	}
+        auS = GetComponent<AudioSource>();
+    }
 
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+        auS.pitch = Random.Range(minPitch, maxPitch);
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (!ps.IsAlive())
             Destroy(gameObject);
 	}
