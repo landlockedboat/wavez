18bfaf04d328664d2f5312a4046805f4a346db0f
wextia <valcazar96@gmail.com>
2017-01-22 12:32:56 +0200

    removed previously .gitignored stuff

--- src/Assets/Scripts/UI/WinTextActivator.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class WinTextActivator : Singleton<WinTextActivator> {
-
-	public void Activate()
-    {
-        transform.GetChild(0).gameObject.SetActive(true);
-    }
-}
